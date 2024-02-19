18e21260ee3d4222f8365eebbe93856b954f0505
hyeonjun <lerelais0102@naver.com>
2024-02-19 15:32:39 +0900

    practice

---
+++ 실습/6. Component Communication/cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <active-user :username="user.name" :userage="user.age"></active-user>
+        <user-data @set-data="setUserData"></user-data>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                user: {
+                    name: 'Max',
+                    age: 31
+                }
+            }
+        },
+        methods: {
+            setUserData(name, age) {
+                this.user = {
+                    name: name,
+                    age: age
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    section{
+        margin: 2rem auto;
+        max-width: 40rem;
+        border-radius: 12px;
+        border: 1px solid #ccc;
+    }
+</style>
---
+++ 실습/6. Component Communication/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <form @submit.prevent="submitData">
+            <p><input type="text" id="username" placeholder="your name" v-model="enteredName"></p>
+            <p><input type="text" id="age" placeholder="your age" v-model="enteredAge"></p>
+            <button>Set User Data</button>
+        </form>
+    </section>
+</template>
+
+<script>
+    export default {
+        emits: ['set-data'],
+        data() {
+            return{
+                enteredName: '',
+                enteredAge: ''
+            }
+        },
+        methods: {
+            submitData(){
+                this.$emit('set-data', this.enteredName, this.enteredAge)
+            },
+        }
+    }
+</script>
